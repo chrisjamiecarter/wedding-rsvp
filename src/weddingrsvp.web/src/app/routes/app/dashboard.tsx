4a63032e22762b71d6022e66329f363a48282fd2
Chris Carter <108958713+chrisjamiecarter@users.noreply.github.com>
2025-03-24 22:01:00 +0000

    Added dashboard route

---
+++ src/weddingrsvp.web/src/app/routes/app/dashboard.tsx
@@ -0,0 +1,10 @@
+const DashboardRoute = () => {
+  return (
+    <section>
+      <h1>Hello from the Dashboard route.</h1>
+      <p>This should only be accessible after login</p>
+    </section>
+  );
+};
+
+export default DashboardRoute;
