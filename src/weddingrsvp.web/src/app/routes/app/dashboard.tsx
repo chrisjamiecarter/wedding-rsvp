7176879428b38c07e8bec2d7cf39ec8ed44f225d
Chris Carter <108958713+chrisjamiecarter@users.noreply.github.com>
2025-04-17 14:14:21 +0100

    Updated dashboard

--- src/weddingrsvp.web/src/app/routes/app/dashboard.tsx
+++ src/weddingrsvp.web/src/app/routes/app/dashboard.tsx
@@ -12,42 +12,6 @@ const DashboardRoute = () => {
         Hello <b>{user.data ? user.data.email : null}</b>
       </h1>
       <p>This page should only be accessible after login</p>
-      <Button
-        onClick={() =>
-          CustomNotifications.info({
-            title: "Heads up!",
-            message: "This is an info notification",
-          })
-        }>
-        Show Info Notification
-      </Button>
-      <Button
-        onClick={() =>
-          CustomNotifications.success({
-            title: "Well done!",
-            message: "This is a success notification",
-          })
-        }>
-        Show Success Notification
-      </Button>
-      <Button
-        onClick={() =>
-          CustomNotifications.warning({
-            title: "Watch out!",
-            message: "This is a warning notification",
-          })
-        }>
-        Show Warning Notification
-      </Button>
-      <Button
-        onClick={() =>
-          CustomNotifications.error({
-            title: "Uh oh!",
-            message: "This is an error notification",
-          })
-        }>
-        Show Error Notification
-      </Button>
     </section>
   );
 };
