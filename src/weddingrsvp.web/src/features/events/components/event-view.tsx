d773928ad80efcb96dff230e15ae881cb98e7b57
Chris Carter <108958713+chrisjamiecarter@users.noreply.github.com>
2025-04-18 13:35:42 +0100

    Added collapsable event data

--- src/weddingrsvp.web/src/features/events/components/event-view.tsx
+++ src/weddingrsvp.web/src/features/events/components/event-view.tsx
@@ -1,10 +1,19 @@
 import { format } from "date-fns";
 import { LoadingPage } from "@/components/ui/loading-page";
 import { useEvent } from "@/features/events/api/get-event";
-import { Group, Table } from "@mantine/core";
+import { Button, Collapse, Group, Table } from "@mantine/core";
 import { UpdateEvent } from "./update-event";
+import { useDisclosure } from "@mantine/hooks";
+import {
+  PanelBottomClose,
+  PanelBottomOpen,
+  PanelTopClose,
+  PanelTopOpen,
+} from "lucide-react";
 
 const EventView = ({ eventId }: { eventId: string }) => {
+  const [opened, { toggle }] = useDisclosure(true);
+
   const eventQuery = useEvent({
     eventId,
   });
@@ -19,53 +28,58 @@ const EventView = ({ eventId }: { eventId: string }) => {
 
   return (
     <>
-      <Group justify="end">
+      <Group justify="space-between">
+        <Button onClick={toggle} variant="transparent" color="dark">
+          {opened ? <PanelTopClose /> : <PanelTopOpen />}
+        </Button>
         <UpdateEvent eventId={eventId} />
       </Group>
 
-      <Table variant="vertical">
-        <Table.Tbody>
-          <Table.Tr>
-            <Table.Th>ID</Table.Th>
-            <Table.Td>{event.id}</Table.Td>
-          </Table.Tr>
+      <Collapse in={opened}>
+        <Table variant="vertical">
+          <Table.Tbody>
+            <Table.Tr>
+              <Table.Th>ID</Table.Th>
+              <Table.Td>{event.id}</Table.Td>
+            </Table.Tr>
 
-          <Table.Tr>
-            <Table.Th>Name</Table.Th>
-            <Table.Td>{event.name}</Table.Td>
-          </Table.Tr>
+            <Table.Tr>
+              <Table.Th>Name</Table.Th>
+              <Table.Td>{event.name}</Table.Td>
+            </Table.Tr>
 
-          <Table.Tr>
-            <Table.Th>Description</Table.Th>
-            <Table.Td>{event.description}</Table.Td>
-          </Table.Tr>
+            <Table.Tr>
+              <Table.Th>Description</Table.Th>
+              <Table.Td>{event.description}</Table.Td>
+            </Table.Tr>
 
-          <Table.Tr>
-            <Table.Th>Venue</Table.Th>
-            <Table.Td>{event.venue}</Table.Td>
-          </Table.Tr>
+            <Table.Tr>
+              <Table.Th>Venue</Table.Th>
+              <Table.Td>{event.venue}</Table.Td>
+            </Table.Tr>
 
-          <Table.Tr>
-            <Table.Th>Address</Table.Th>
-            <Table.Td>{event.address}</Table.Td>
-          </Table.Tr>
+            <Table.Tr>
+              <Table.Th>Address</Table.Th>
+              <Table.Td>{event.address}</Table.Td>
+            </Table.Tr>
 
-          <Table.Tr>
-            <Table.Th>Date</Table.Th>
-            <Table.Td>{format(event.date, "PPPP")}</Table.Td>
-          </Table.Tr>
+            <Table.Tr>
+              <Table.Th>Date</Table.Th>
+              <Table.Td>{format(event.date, "PPPP")}</Table.Td>
+            </Table.Tr>
 
-          <Table.Tr>
-            <Table.Th>Time</Table.Th>
-            <Table.Td>{event.time}</Table.Td>
-          </Table.Tr>
+            <Table.Tr>
+              <Table.Th>Time</Table.Th>
+              <Table.Td>{event.time}</Table.Td>
+            </Table.Tr>
 
-          <Table.Tr>
-            <Table.Th>Dress Code</Table.Th>
-            <Table.Td>{event.dressCode}</Table.Td>
-          </Table.Tr>
-        </Table.Tbody>
-      </Table>
+            <Table.Tr>
+              <Table.Th>Dress Code</Table.Th>
+              <Table.Td>{event.dressCode}</Table.Td>
+            </Table.Tr>
+          </Table.Tbody>
+        </Table>
+      </Collapse>
     </>
   );
 };
